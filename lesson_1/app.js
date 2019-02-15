597689f2ea07b83df80d0feb843aad63397d22de
Chris Froehlingsdorf <chris@5cf.de>
2019-02-15 16:27:05 +0100

    better lessons 1 2

--- lesson_1/app.js
+++ lesson_1/app.js
@@ -1,8 +1,15 @@
 // lesson 1: assertions
 
+// old way
+var myVariable = "bla";
+
+// new way
 let x = null;
 x = "geht das";
 x = 12;
 x = "ja das geht";
+
+// constant variable (reference not changeable)
+const myVariable2 = "cannot be changed";
 
 console.log(x);
--- lesson_2/app.js
+++ lesson_2/app.js
@@ -1,4 +1,16 @@
 // lesson 1: types
 
+const aBoolean = true; // false
 const aNumber = 1;
 const aString = "abcd";
+const aDecimal = 1.2;
+const anArray = [];
+const anObject = {};
+const aNull = null;
+const anUndefined = undefined;
+
+console.log(aBoolean, aNumber, aString, aDecimal, anArray, anObject, aNull, anUndefined);
+
+// typeof operator
+
+console.log(typeof aString, typeof anObject, typeof aNull, typeof aDecimal, typeof aNumber);
